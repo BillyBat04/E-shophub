d5a2070d023f1168dc57575b6dc9db4aecd03636
BillyBat04 <khoafd11@gmail.com>
2025-01-15 14:25:24 +0700

    Update formatNumber.js

--- src/helpers/formatNumber.js
+++ src/helpers/formatNumber.js
@@ -1,7 +1,12 @@
 function formatNumber(number) {
-    let numberStr = number.toString();
-    
+    if (typeof number !== 'number') {
+        throw new Error('Input must be a number');
+    }
+
+    // Chuyển số thành chuỗi và sử dụng regex để thêm dấu "."
+    const numberStr = number.toFixed(0); // Đảm bảo không có phần thập phân
     const regex = /(\d)(?=(\d{3})+(?!\d))/g;
-    return numberStr.replace(regex, '$1.');
+    return numberStr.replace(regex, '$1.') + ' VND';
 }
-export default formatNumber
+
+export default formatNumber;
